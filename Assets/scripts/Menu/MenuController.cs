a394cd40ffbf71eaad3e477360000ebb10c859f8
berme <diegodecastro@usal.es>
2024-11-10 16:25:09 +0100

    añadido menu principal y opciones

--- Assets/scripts/Menu/MenuController.cs
+++ Assets/scripts/Menu/MenuController.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+
+    [Header("Configuracion de la aplicacion")]
+    public Slider volumenJuego;
+    public Toggle mute;
+
+    public AudioMixer audioMixer;
+
+   
+
+
+    private void Awake()
+    {
+        volumenJuego.onValueChanged.AddListener(delegate { CambiarVolumen(); });
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +40,32 @@ public class MenuController : MonoBehaviour
     {
         Debug.Log("Saliendo de la aplicacion");
         Application.Quit();
+    }
+
+
+    public void CargarEscena(string nombreEscena)
+    {
+        SceneManager.LoadScene(nombreEscena);
+    }
+
+
+
+    public void CambiarVolumen()
+    {
+        audioMixer.SetFloat("Musica", volumenJuego.value);
+    }
+
+
+    public void Mute()
+    {
+        if (mute.isOn)
+        {
+            audioMixer.SetFloat("Musica", -80);
+        }
+        else
+        {
+            audioMixer.SetFloat("Musica", volumenJuego.value);
+        }
     }
 
 
